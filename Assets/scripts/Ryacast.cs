ee53c41307673be33fd6151e50be0c5ae4e484fe
HugeLMoy <lucymoynihan17@icloud.com>
2025-05-15 15:50:27 +0100

    end scene

--- Assets/scripts/Ryacast.cs
+++ Assets/scripts/Ryacast.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RaycastExample : MonoBehaviour
 {
@@ -24,6 +25,10 @@ public class RaycastExample : MonoBehaviour
         SmokeUp.SetActive(false);
     }
 
+    IEnumerator Timer() 
+    {
+        yield return new WaitForSeconds(5);
+    }
     void Update()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -68,6 +73,8 @@ public class RaycastExample : MonoBehaviour
                 if (hit.collider.name == "Lighter")
                     {
                         Smoke.SetActive(true);
+                        StartCoroutine(Timer());
+                        SceneManager.LoadSceneAsync(2);
                     }
 
             }
